e79a4b2c5fd5318ffe9a97551ee123db9de84834
Paul Novaes <n3times@users.noreply.github.com>
2022-07-10 14:48:29 -0700

    Create MenuBarView.

---
+++ ios/rcl57/MenuBarView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuBarView.swift
+//  rcl57
+//
+//  Created by Paul Novaes on 7/10/22.
+//
+
+import SwiftUI
+
+struct MenuBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBarView()
+    }
+}
